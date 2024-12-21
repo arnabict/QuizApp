9f0e154a2ad6b1a6efe219f513261d8e34af5632
Arnab Paul <arnabict@outlook.com>
2024-12-21 22:07:35 +0100

    quiz questions answers progress setup && route setup

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
+import PrivateOutlet from "./PrivateOutlet";
+import PublicOutlet from "./PublicOutlet";
 
 function App() {
   return (
@@ -15,10 +17,18 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+            <Route path="/signup" element={<PublicOutlet />}>
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+            <Route path="/login" element={<PublicOutlet />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="/quiz/:id" element={<PrivateOutlet />}>
+              <Route path="/quiz/:id" element={<Quiz />} />
+            </Route>
+            <Route path="/result/:id" element={<PrivateOutlet />}>
+              <Route path="/result/:id" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -46,7 +46,6 @@ export default function useVideoList(page) {
         setError(true);
       }
     }
-
     fetchVideos();
   }, [page]);
 
